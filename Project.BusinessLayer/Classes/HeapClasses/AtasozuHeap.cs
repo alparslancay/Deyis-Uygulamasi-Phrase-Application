98a363c955bed5185a5d80b8ac41083e6357e513
Hamdi DAMAR <39102004+hamdidamar@users.noreply.github.com>
2019-07-11 00:04:23 +0300

    Update AtasozuHeap

--- Project.BusinessLayer/Classes/HeapClasses/AtasozuHeap.cs
+++ Project.BusinessLayer/Classes/HeapClasses/AtasozuHeap.cs
@@ -11,29 +11,105 @@ namespace Project.BusinessLayer.Classes.HeapClasses
 {
     public class AtasozuHeap : HeapADT<Atasozu>
     {
+       private int currentSize;
+        public AtasozuHeap(List<Atasozu> AtasozuHeapArray)
+        {
+            currentSize = 0;
+            agacDugumleri = AtasozuHeapArray;
+        }
         public override Atasozu Ara(Predicate<Atasozu> predicate)
         {
-            throw new NotImplementedException();
+            return agacDugumleri.Find(predicate);
         }
 
         public override void Ekle(Atasozu entity)
         {
-            throw new NotImplementedException();
+            agacDugumleri[currentSize] = entity;
+            MoveToUpAtasozu(currentSize++);
         }
 
         public override void Guncelle(Atasozu entity)
         {
-            throw new NotImplementedException();
+            Predicate<Atasozu> predicate = guncellenecekAtasozu => guncellenecekAtasozu.Id == entity.Id;
+            Atasozu newAtasozu = new Atasozu();
+            newAtasozu = Ara(predicate);
+            newAtasozu = entity;
         }
 
         public override void Sil(Atasozu entity)
         {
-            throw new NotImplementedException();
+            for (int index = 0; index < agacDugumleri.Count; index++)
+            {
+                if (agacDugumleri[index].Id == entity.Id)
+                {
+                    Remove(index);
+                    break;
+                }
+            }
         }
 
         public override List<Atasozu> TumElemanList()
         {
-            throw new NotImplementedException();
+            List<Atasozu> yedekListAtasozuHeap = new List<Atasozu>();
+
+            for (int i = 0; i < agacDugumleri.Count; i++)
+            {
+                yedekListAtasozuHeap.Add(agacDugumleri[i]);
+            }
+            List<Atasozu> siraliListAtasozuHeap = new List<Atasozu>();
+            while (!IsEmpty())
+            {
+                siraliListAtasozuHeap.Add(Remove(0));
+
+            }
+            agacDugumleri = yedekListAtasozuHeap;
+            return siraliListAtasozuHeap;
+
+        }
+        private void MoveToUpAtasozu(int index)
+        {
+            int parent = (index - 1) / 2;
+            Atasozu bottom = agacDugumleri[index];
+            while (string.Compare(agacDugumleri[parent].DeyisCumle.ToString(), bottom.DeyisCumle.ToString()) == -1)
+            {
+                agacDugumleri[index] = agacDugumleri[parent];
+                index = parent;
+                parent = (parent - 1) / 2;
+            }
+            agacDugumleri[index] = bottom;
+        }
+        private void MoveToDownAtasozu(int index)
+        {
+            int largerChild;
+            Atasozu top = agacDugumleri[index];
+            while (index < currentSize / 2)
+            {
+                int leftChild = 2 * index + 1;
+                int rightChild = leftChild + 1;
+
+                if (rightChild < currentSize && string.Compare(agacDugumleri[leftChild].DeyisCumle, agacDugumleri[rightChild].DeyisCumle) == -1)
+                    largerChild = rightChild;
+                else
+                    largerChild = leftChild;
+                if (string.Compare(top.DeyisCumle, agacDugumleri[largerChild].DeyisCumle) == 1)
+                    break;
+                agacDugumleri[index] = agacDugumleri[largerChild];
+                index = largerChild;
+            }
+            agacDugumleri[index] = top;
+        }
+        private Atasozu Remove(int index)
+        {
+            Atasozu root = agacDugumleri[index];
+            agacDugumleri[index] = agacDugumleri[--currentSize];
+            MoveToDownAtasozu(index);
+            return root;
+        }
+
+        private bool IsEmpty()
+        {
+            return currentSize == 0;
+
         }
     }
 }
