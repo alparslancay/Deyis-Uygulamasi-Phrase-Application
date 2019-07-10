39f381e78523b30269ec383a5db2a91d8120ab9b
Hamdi DAMAR <39102004+hamdidamar@users.noreply.github.com>
2019-07-10 23:59:21 +0300

    Update YansimaHeap

--- Project.BusinessLayer/Classes/HeapClasses/YansimaHeap.cs
+++ Project.BusinessLayer/Classes/HeapClasses/YansimaHeap.cs
@@ -11,29 +11,105 @@ namespace Project.BusinessLayer.Classes.HeapClasses
 {
     public class YansimaHeap : HeapADT<Yansima>
     {
+        private int currentSize;
+        public YansimaHeap(List<Yansima> YansimaHeapArray)
+        {
+            currentSize = 0;
+            agacDugumleri = YansimaHeapArray;
+        }
         public override Yansima Ara(Predicate<Yansima> predicate)
         {
-            throw new NotImplementedException();
+            return agacDugumleri.Find(predicate);
         }
 
         public override void Ekle(Yansima entity)
         {
-            throw new NotImplementedException();
+            agacDugumleri[currentSize] = entity;
+            MoveToUpYansima(currentSize++);
         }
 
         public override void Guncelle(Yansima entity)
         {
-            throw new NotImplementedException();
+            Predicate<Yansima> predicate = guncellenecekYansima => guncellenecekYansima.Id == entity.Id;
+            Yansima newYansima = new Yansima();
+            newYansima = Ara(predicate);
+            newYansima = entity;
         }
 
         public override void Sil(Yansima entity)
         {
-            throw new NotImplementedException();
+            for (int index = 0; index < agacDugumleri.Count; index++)
+            {
+                if (agacDugumleri[index].Id == entity.Id)
+                {
+                    Remove(index);
+                    break;
+                }
+            }
         }
 
         public override List<Yansima> TumElemanList()
         {
-            throw new NotImplementedException();
+            List<Yansima> yedekListYansimaHeap = new List<Yansima>();
+
+            for (int i = 0; i < agacDugumleri.Count; i++)
+            {
+                yedekListYansimaHeap.Add(agacDugumleri[i]);
+            }
+            List<Yansima> siraliListYansimaHeap = new List<Yansima>();
+            while (!IsEmpty())
+            {
+                siraliListYansimaHeap.Add(Remove(0));
+
+            }
+            agacDugumleri = yedekListYansimaHeap;
+            return siraliListYansimaHeap;
+
+        }
+        private void MoveToUpYansima(int index)
+        {
+            int parent = (index - 1) / 2;
+            Yansima bottom = agacDugumleri[index];
+            while (string.Compare(agacDugumleri[parent].DeyisCumle.ToString(), bottom.DeyisCumle.ToString()) == -1)
+            {
+                agacDugumleri[index] = agacDugumleri[parent];
+                index = parent;
+                parent = (parent - 1) / 2;
+            }
+            agacDugumleri[index] = bottom;
+        }
+        private void MoveToDownYansima(int index)
+        {
+            int largerChild;
+            Yansima top = agacDugumleri[index];
+            while (index < currentSize / 2)
+            {
+                int leftChild = 2 * index + 1;
+                int rightChild = leftChild + 1;
+
+                if (rightChild < currentSize && string.Compare(agacDugumleri[leftChild].DeyisCumle, agacDugumleri[rightChild].DeyisCumle) == -1)
+                    largerChild = rightChild;
+                else
+                    largerChild = leftChild;
+                if (string.Compare(top.DeyisCumle, agacDugumleri[largerChild].DeyisCumle) == 1)
+                    break;
+                agacDugumleri[index] = agacDugumleri[largerChild];
+                index = largerChild;
+            }
+            agacDugumleri[index] = top;
+        }
+        private Yansima Remove(int index)
+        {
+            Yansima root = agacDugumleri[index];
+            agacDugumleri[index] = agacDugumleri[--currentSize];
+            MoveToDownYansima(index);
+            return root;
+        }
+
+        private bool IsEmpty()
+        {
+            return currentSize == 0;
+
         }
     }
 }
