09d4cec00dbc24193f40bc01e176a11e5621db5d
Hamdi DAMAR <39102004+hamdidamar@users.noreply.github.com>
2019-07-11 00:01:08 +0300

    Update IkilemeIslemleri

--- Project.BusinessLayer/Classes/HeapClasses/IkilemeHeap.cs
+++ Project.BusinessLayer/Classes/HeapClasses/IkilemeHeap.cs
@@ -11,29 +11,105 @@ namespace Project.BusinessLayer.Classes.HeapClasses
 {
     public class IkilemeHeap : HeapADT<Ikileme>
     {
+       private int currentSize;
+        public IkilemeHeap(List<Ikileme> IkilemeHeapArray)
+        {
+            currentSize = 0;
+            agacDugumleri = IkilemeHeapArray;
+        }
         public override Ikileme Ara(Predicate<Ikileme> predicate)
         {
-            throw new NotImplementedException();
+            return agacDugumleri.Find(predicate);
         }
 
         public override void Ekle(Ikileme entity)
         {
-            throw new NotImplementedException();
+            agacDugumleri[currentSize] = entity;
+            MoveToUpIkileme(currentSize++);
         }
 
         public override void Guncelle(Ikileme entity)
         {
-            throw new NotImplementedException();
+            Predicate<Ikileme> predicate = guncellenecekIkileme => guncellenecekIkileme.Id == entity.Id;
+            Ikileme newIkileme = new Ikileme();
+            newIkileme = Ara(predicate);
+            newIkileme = entity;
         }
 
         public override void Sil(Ikileme entity)
         {
-            throw new NotImplementedException();
+            for (int index = 0; index < agacDugumleri.Count; index++)
+            {
+                if (agacDugumleri[index].Id == entity.Id)
+                {
+                    Remove(index);
+                    break;
+                }
+            }
         }
 
         public override List<Ikileme> TumElemanList()
         {
-            throw new NotImplementedException();
+            List<Ikileme> yedekListIkilemeHeap = new List<Ikileme>();
+
+            for (int i = 0; i < agacDugumleri.Count; i++)
+            {
+                yedekListIkilemeHeap.Add(agacDugumleri[i]);
+            }
+            List<Ikileme> siraliListIkilemeHeap = new List<Ikileme>();
+            while (!IsEmpty())
+            {
+                siraliListIkilemeHeap.Add(Remove(0));
+
+            }
+            agacDugumleri = yedekListIkilemeHeap;
+            return siraliListIkilemeHeap;
+
+        }
+        private void MoveToUpIkileme(int index)
+        {
+            int parent = (index - 1) / 2;
+            Ikileme bottom = agacDugumleri[index];
+            while (string.Compare(agacDugumleri[parent].DeyisCumle.ToString(), bottom.DeyisCumle.ToString()) == -1)
+            {
+                agacDugumleri[index] = agacDugumleri[parent];
+                index = parent;
+                parent = (parent - 1) / 2;
+            }
+            agacDugumleri[index] = bottom;
+        }
+        private void MoveToDownIkileme(int index)
+        {
+            int largerChild;
+            Ikileme top = agacDugumleri[index];
+            while (index < currentSize / 2)
+            {
+                int leftChild = 2 * index + 1;
+                int rightChild = leftChild + 1;
+
+                if (rightChild < currentSize && string.Compare(agacDugumleri[leftChild].DeyisCumle, agacDugumleri[rightChild].DeyisCumle) == -1)
+                    largerChild = rightChild;
+                else
+                    largerChild = leftChild;
+                if (string.Compare(top.DeyisCumle, agacDugumleri[largerChild].DeyisCumle) == 1)
+                    break;
+                agacDugumleri[index] = agacDugumleri[largerChild];
+                index = largerChild;
+            }
+            agacDugumleri[index] = top;
+        }
+        private Ikileme Remove(int index)
+        {
+            Ikileme root = agacDugumleri[index];
+            agacDugumleri[index] = agacDugumleri[--currentSize];
+            MoveToDownIkileme(index);
+            return root;
+        }
+
+        private bool IsEmpty()
+        {
+            return currentSize == 0;
+
         }
     }
 }
