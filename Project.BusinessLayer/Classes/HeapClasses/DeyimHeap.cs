44304e94e43076755c3ec1ccbf93e71334997a4e
Hamdi DAMAR <39102004+hamdidamar@users.noreply.github.com>
2019-07-11 00:03:29 +0300

    Update DeyimHeap

--- Project.BusinessLayer/Classes/HeapClasses/DeyimHeap.cs
+++ Project.BusinessLayer/Classes/HeapClasses/DeyimHeap.cs
@@ -11,29 +11,105 @@ namespace Project.BusinessLayer.Classes.HeapClasses
 {
     public class DeyimHeap : HeapADT<Deyim>
     {
+        private int currentSize;
+        public DeyimHeap(List<Deyim> DeyimHeapArray)
+        {
+            currentSize = 0;
+            agacDugumleri = DeyimHeapArray;
+        }
         public override Deyim Ara(Predicate<Deyim> predicate)
         {
-            throw new NotImplementedException();
+            return agacDugumleri.Find(predicate);
         }
 
         public override void Ekle(Deyim entity)
         {
-            throw new NotImplementedException();
+            agacDugumleri[currentSize] = entity;
+            MoveToUpDeyim(currentSize++);
         }
 
         public override void Guncelle(Deyim entity)
         {
-            throw new NotImplementedException();
+            Predicate<Deyim> predicate = guncellenecekDeyim => guncellenecekDeyim.Id == entity.Id;
+            Deyim newDeyim = new Deyim();
+            newDeyim = Ara(predicate);
+            newDeyim = entity;
         }
 
         public override void Sil(Deyim entity)
         {
-            throw new NotImplementedException();
+            for (int index = 0; index < agacDugumleri.Count; index++)
+            {
+                if (agacDugumleri[index].Id == entity.Id)
+                {
+                    Remove(index);
+                    break;
+                }
+            }
         }
 
         public override List<Deyim> TumElemanList()
         {
-            throw new NotImplementedException();
+            List<Deyim> yedekListDeyimHeap = new List<Deyim>();
+
+            for (int i = 0; i < agacDugumleri.Count; i++)
+            {
+                yedekListDeyimHeap.Add(agacDugumleri[i]);
+            }
+            List<Deyim> siraliListDeyimHeap = new List<Deyim>();
+            while (!IsEmpty())
+            {
+                siraliListDeyimHeap.Add(Remove(0));
+
+            }
+            agacDugumleri = yedekListDeyimHeap;
+            return siraliListDeyimHeap;
+
+        }
+        private void MoveToUpDeyim(int index)
+        {
+            int parent = (index - 1) / 2;
+            Deyim bottom = agacDugumleri[index];
+            while (string.Compare(agacDugumleri[parent].DeyisCumle.ToString(), bottom.DeyisCumle.ToString()) == -1)
+            {
+                agacDugumleri[index] = agacDugumleri[parent];
+                index = parent;
+                parent = (parent - 1) / 2;
+            }
+            agacDugumleri[index] = bottom;
+        }
+        private void MoveToDownDeyim(int index)
+        {
+            int largerChild;
+            Deyim top = agacDugumleri[index];
+            while (index < currentSize / 2)
+            {
+                int leftChild = 2 * index + 1;
+                int rightChild = leftChild + 1;
+
+                if (rightChild < currentSize && string.Compare(agacDugumleri[leftChild].DeyisCumle, agacDugumleri[rightChild].DeyisCumle) == -1)
+                    largerChild = rightChild;
+                else
+                    largerChild = leftChild;
+                if (string.Compare(top.DeyisCumle, agacDugumleri[largerChild].DeyisCumle) == 1)
+                    break;
+                agacDugumleri[index] = agacDugumleri[largerChild];
+                index = largerChild;
+            }
+            agacDugumleri[index] = top;
+        }
+        private Deyim Remove(int index)
+        {
+            Deyim root = agacDugumleri[index];
+            agacDugumleri[index] = agacDugumleri[--currentSize];
+            MoveToDownDeyim(index);
+            return root;
+        }
+
+        private bool IsEmpty()
+        {
+            return currentSize == 0;
+
         }
     }
 }
