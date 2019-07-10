dfc92598ef7fde6f987c0eaee94331e16f3c9bce
Hamdi DAMAR <39102004+hamdidamar@users.noreply.github.com>
2019-07-10 22:21:16 +0300

    Update AtasozuIslemleri

--- Project.BusinessLayer/Classes/AtasozuIslemleri.cs
+++ Project.BusinessLayer/Classes/AtasozuIslemleri.cs
@@ -11,8 +11,7 @@ using System.Threading.Tasks;
 namespace Project.BusinessLayer.Classes
 {
     public class AtasozuIslemleri : DeyisIslemleriADT<Atasozu>
-    {
-        public AtasozuIslemleri()
+    { public AtasozuIslemleri()
         {
             unitOfWork = new UnitOfWork(new ProjectDbContext("DeyisDB"));
             heapADT = new AtasozuHeap();
@@ -20,32 +19,74 @@ namespace Project.BusinessLayer.Classes
 
         public override Atasozu CumleAra(string deyisCumle)
         {
-            throw new NotImplementedException();
+            deyisCumle = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(deyisCumle);
+            deyisCumle = deyisCumle.ToUpper();
+            Expression<Func<Atasozu, bool>> predicate = arananAtasozu => arananAtasozu.DeyisCumle == deyisCumle;
+            IEnumerable<Atasozu> istenenAtasozu = unitOfWork.Atasozleri.Find(predicate);
+            return istenenAtasozu.First();
         }
 
         public override bool Ekle(Atasozu entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                unitOfWork.Atasozleri.Add(entity);
+                unitOfWork.Complete();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
         public override bool Guncelle(Atasozu entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Atasozu eskiAtasozu = new Atasozu();
+                eskiAtasozu = unitOfWork.Atasozleri.Get(entity.Id);
+                eskiAtasozu = entity;
+                unitOfWork.Complete();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
         public override Atasozu IDAra(int deyisID)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Expression<Func<Atasozu, bool>> predicate = arananAtasozu => arananAtasozu.Id == deyisID;
+                IEnumerable<Atasozu> istenenAtasozu = unitOfWork.Atasozleri.Find(predicate);
+                return istenenAtasozu.First();
+            }
+            catch (Exception)
+            {
+                return null;
+            }
         }
 
         public override bool Sil(Atasozu entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                unitOfWork.Atasozleri.Remove(entity);
+                unitOfWork.Complete();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
         public override List<Atasozu> TumElemanList()
         {
-            throw new NotImplementedException();
+            return unitOfWork.Atasozleri.GetAll().ToList();
         }
     }
 }
